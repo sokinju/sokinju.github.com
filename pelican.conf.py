bb9ba3c8c956f043b906b312a647c4d616536b74
sokinju <suqi.wang.nju@gmail.com>
2012-06-20 12:40:43 +0800

    add blogs...

---
+++ pelican.conf.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"soki"
+SITENAME = u"see the world"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+DISQUS_SITENAME = 'sokinju'
+SATIC_PATHS = ['./images', ]
+
+
+
+
+    
---
+++ src/hello-world.md
@@ -0,0 +1,5 @@
+title: markdown + pelican + github = Perfect blog platform
+slug: pelican-github-markdown
+date: 2012-6-9
+category: 技术
+tags: markdown, pelican, github
